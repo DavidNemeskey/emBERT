09588d8cb8a99a2d6918fda47ad3dfd60e286a36
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2020-11-17 00:10:59 +0100

    extract_transitions3.

--- embert/extract_transitions.py
+++ embert/extract_transitions.py
@@ -37,6 +37,41 @@ def extract_transitions2(processor, label_map):
     return init_stats, transitions
 
 
+def extract_transitions3(processor):
+    """Extracts the label->label transition matrix."""
+    label_map = {label: i for i, label in enumerate(processor.get_labels())
+                 if not label.startswith('[')}
+    init_stats = np.zeros(len(label_map), dtype=float)
+    transitions = np.zeros((len(label_map), len(label_map)), dtype=float)
+    for _, labels in processor.reader(os.path.join(processor.data_dir, 'train.txt')):
+        init_stats[label_map[labels[0]]] += 1
+        for l1, l2 in pairwise(labels):
+            transitions[label_map[l1], label_map[l2]] += 1
+    return init_stats, transitions
+
+
+def main3():
+    import sys
+    processor_cls = get_processor(sys.argv[2])
+    processor = processor_cls(sys.argv[1], get_format_reader('tsv'))
+    idx2label = processor.get_labels()
+    label2idx = {label: i for i, label in enumerate(idx2label)
+                 if not label.startswith('[')}
+    init_stats, transitions = extract_transitions2(processor)
+    print('INIT:\n')
+    init_norm = init_stats / sum(init_stats)
+    for idx, v in enumerate(init_stats):
+        if v != 0:
+            print(f'{idx2label[idx]}: {v} ({init_norm[idx]})')
+
+    print('\n\n\nTRANSITIONS:\n')
+    for l1, trans in enumerate(transitions):
+        tr_sum = sum(trans)
+        for l2, v in enumerate(trans):
+            if v != 0:
+                print(f'{idx2label[l1]} -> {idx2label[l2]}: {v} ({v / tr_sum})')
+
+
 def main2():
     import sys
     processor_cls = get_processor(sys.argv[2])
@@ -75,4 +110,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main2()
+    main3()
