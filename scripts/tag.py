d81229e068012211ede54113e1d0dda122a0723e
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2021-12-02 01:38:46 +0000

    tag.py now handles dictionary hierarchies as input using DataProcessing.all_files_from_directory().

--- scripts/tag.py
+++ scripts/tag.py
@@ -9,11 +9,13 @@ selected model. The model is selected by its alias in emBERT.
 import argparse
 import logging
 import os
+import os.path as op
 
 from tqdm import tqdm
 
 from embert.data_format import all_formats, get_format_reader
 from embert.embert import EmBERT
+from embert.processors import DataProcessor
 
 
 def parse_arguments():
@@ -31,7 +33,7 @@ def parse_arguments():
                              'file must exist in the configs directory and '
                              'the corresponding model must have been already '
                              'downloaded to the models directory.')
-    parser.add_argument('--data_format', required=True, choices=all_formats(),
+    parser.add_argument('--data-format', required=True, choices=all_formats(),
                         help='The data format of the input files.')
     return parser.parse_args()
 
@@ -44,17 +46,22 @@ def main():
         datefmt='%m/%d/%Y %H:%M:%S', level=logging.DEBUG
     )
 
-    if not os.path.exists(args.output_dir):
+    if not op.exists(args.output_dir):
         os.makedirs(args.output_dir)
 
     em = EmBERT(task=args.task_name)
     reader = get_format_reader(args.data_format)
-    for input_file in tqdm(os.listdir(args.input_dir), desc='Files'):
-        with open(os.path.join(args.output_dir, input_file), 'wt') as outf:
-            for sentence, labels in tqdm(
-                reader(os.path.join(args.input_dir, input_file)),
-                desc='Sentences'
-            ):
+    # for input_file in tqdm(os.listdir(args.input_dir), desc='Files'):
+    for input_file in tqdm(
+        DataProcessor.all_files_from_directory(args.input_dir),
+        desc='Files'
+    ):
+        output_file = op.join(args.output_dir,
+                              op.relpath(input_file, start=args.input_dir))
+        if not op.isdir(output_dir := op.dirname(output_file)):
+            os.makedirs(output_dir)
+        with open(output_file, 'wt') as outf:
+            for sentence, labels in tqdm(reader(input_file), desc='Sentences'):
                 tagged = em.process_sentence([[w] for w in sentence], [0])
                 for token in tagged:
                     print('\t'.join(token), file=outf)
